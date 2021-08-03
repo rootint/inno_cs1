22b909cac71a50dcf2c3b5129062afbc899d88c9
RandoM <danik.tim@gmail.com>
2021-08-03 14:55:43 +0300

    initial commit

---
+++ 1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+
+bool is_prime(int n) {
+    if (n < 2) {
+        return false;
+    }
+    for (int i = 2; i <= sqrt(n); i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    int n;
+    std::string f = "wtf";
+    std::cout << f << std::endl;
+    std::cout << "Enter the number: ";
+    std::cin >> n;
+    do {
+        std::cout << "Enter the number: ";
+        std::cin >> n;  
+    } while (n < 0);
+    std::cout << ((is_prime(n)) ? "Prime" : "Not Prime") << std::endl;
+    return 0;
+}
